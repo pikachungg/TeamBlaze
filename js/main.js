abd06aa2fc699eb19d40145b91dc50b18c7e0caf
pikachungg <34949943+pikachungg@users.noreply.github.com>
2019-06-23 22:46:50 -0400

    Fixes
    Small Fixes

--- js/main.js
+++ js/main.js
@@ -14,9 +14,6 @@
 
 	$WIN = $(window);
 	
-
-   // Add the User Agent to the <html>
-   // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
 	var doc = document.documentElement;
 	doc.setAttribute('data-useragent', navigator.userAgent);
 
